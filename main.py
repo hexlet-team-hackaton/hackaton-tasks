ba3bbae6906cddf556eee6a41a81d6b851ddbbc1
Mikhail Orlov <crazyminer398@gmail.com>
2022-12-23 17:34:31 +0300

    get data of first task

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+import json
+
+
+data = requests.get('https://datsanta.dats.team/json/map/faf7ef78-41b3-4a36-8423-688a61929c08.json')
+
+with open('result.json', 'w') as f:
+    json.dump(data.json(), f, indent=4, ensure_ascii=False)
